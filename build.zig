dd02d29d7a33fb8f882c915d9ce0fb2b4f89e950
Tom <tommmmm@protonmail.com>
2023-08-27 21:27:42 +0100

    Initial commit

---
+++ build.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Add your executable and configure with
+    // target and optimize, specify your root file (ex. main.zig)
+    const exe = b.addExecutable(.{
+        .name = "zig-mesh",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Add the mach_glfw dependency, note the name here
+    // should match the name in your build.zig.zon
+    const glfw_dep = b.dependency("mach_glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Add the module to our package scope
+    // Note the name here is the module that
+    // you will import (`@import("mach-glfw")`)
+    exe.addModule("mach-glfw", glfw_dep.module("mach-glfw"));
+
+    // Use the mach-glfw .link helper here
+    // to link the glfw library for us
+    try @import("mach_glfw").link(b, exe);
+
+    // Same as above for our gl module,
+    // because we copied the gl code into the project
+    // we instead just create the module inline
+    exe.addModule("gl", b.createModule(.{
+        .source_file = .{ .path = "libs/gl41.zig" },
+    }));
+
+    // Once all is done, we install our artifact which
+    // in this case is our executable
+    b.installArtifact(exe);
+
+    // This is basic boilerplate from zig's stock build.zig,
+    // We add a run step so we can run `zig build run` to
+    // execute our program after building
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    // Similar to the above but this adds tests
+    // and a test step 'zig build test'
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+const log = std.log.scoped(.Engine);
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
+
+/// Default GLFW error handling callback
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw: {}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    // Create our window
+    const window = glfw.Window.create(1600, 900, "mach-glfw + zig-opengl", null, null, .{
+        .opengl_profile = .opengl_core_profile,
+        .context_version_major = 4,
+        .context_version_minor = 0,
+    }) orelse {
+        std.log.err("failed to create GLFW window: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    };
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
+    // Wait for the user to close the window.
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+
+        gl.clearColor(0.1, 0, 0.2, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        window.swapBuffers();
+    }
+}
